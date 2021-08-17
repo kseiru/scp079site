b2d837455513772adb8738588dc295a80ce7743a
Alexey <87545069+kseiru@users.noreply.github.com>
2021-08-18 00:29:05 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,39 @@
+<!doctype html>
+<html>
+    <head>
+        <title>SCP-079 Database</title>
+        <link rel="icon" href="files/icon.ico">
+    </head>
+    <style>
+        body {
+            background-color: black;
+        }
+
+        .scpnumber {
+            font-size: 72px;
+            font-style: oblique;
+            color: darkred;
+        }
+        
+        .scpclass {
+            font-size: 32px;
+            font-style: italic;
+            color: darkred;
+        }
+
+        .scpdescription {
+            font-size: 16px;
+            color: darkred;
+        }
+    </style>
+    <body>
+        <p class="scpnumber">
+            <?php
+            $scp001name = 'SCP-001 | ЗАСЕКРЕЧЕНО';
+            $scp001class = 'КЛАСС: ТАУМИЭЛЬ';
+            $scp001desc = 'ЗАСЕКРЕЧЕНАЯ ИНФОРМАЦИЯ';
+            echo($scp001name, <br> $scp001class, <br> $cp001desc);
+            ?>
+        </p>
+    </body>
+</html>
